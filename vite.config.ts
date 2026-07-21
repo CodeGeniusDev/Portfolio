be2df5c801c9421d83531fc77c23d50a4bb90ae1
Abdullah Abbad <codegenius.inc@gmail.com>
2026-07-22 01:20:05 +0500

    fix build logs

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,15 @@ import { defineConfig } from 'vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import netlify from '@netlify/vite-plugin-tanstack-start'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     tanstackStart(),
-    // react's vite plugin must come after start's vite plugin
     react(),
     tailwindcss(),
+    netlify(),
   ],
   resolve: {
     alias: {
