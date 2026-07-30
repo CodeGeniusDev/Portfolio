1d5093a1ae8cc07c27a418777d600b9fafe57cda
Abdullah Abbad <codegenius.inc@gmail.com>
2026-07-31 01:23:51 +0500

    add PWA feature

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import netlify from '@netlify/vite-plugin-tanstack-start'
-import path from 'path'
+import { defineConfig } from "vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+import { VitePWA } from "vite-plugin-pwa";
+import path from "path";
 
 export default defineConfig({
   plugins: [
@@ -11,10 +12,36 @@ export default defineConfig({
     react(),
     tailwindcss(),
     netlify(),
+
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Muhammad Abdullah Abbad",
+        short_name: "Abdullah",
+        description: "Full Stack Web Developer Portfolio",
+        theme_color: "#AFCD5F",
+        background_color: "#212121",
+        display: "standalone",
+
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "192x192",
+            type: "image/x-icon",
+          },
+          {
+            src: "/favicon.ico",
+            sizes: "512x512",
+            type: "image/x-icon",
+          },
+        ],
+      },
+    }),
   ],
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
