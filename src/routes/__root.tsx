765d29faed9ee01deb48087585edd8ce2cd24631
Abdullah Abbad <codegenius.inc@gmail.com>
2026-07-31 23:20:04 +0500

    add google analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -167,6 +167,23 @@ function RootShell({ children }: { children: ReactNode }) {
     <html lang="en">
       <head>
         <HeadContent />
+
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-C08HKEQ509"
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-C08HKEQ509');
+      `,
+          }}
+        />
+
       </head>
       <body>
         {children}
